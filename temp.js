776072811f07058350774036d3f6083cbf88aa9b
anonymus1234-cloud <84515544+anonymus1234-cloud@users.noreply.github.com>
2023-12-02 00:51:23 -0500

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,5 @@
+var a=Number(prompt("Enter temperature in Celsius"))
+function fahrenhiet(){
+    var fahr=(a*(9/5))+32
+    document.getElementById("a").innerHTML="The Fahrenhiet Temprature is "+fahr
+}
